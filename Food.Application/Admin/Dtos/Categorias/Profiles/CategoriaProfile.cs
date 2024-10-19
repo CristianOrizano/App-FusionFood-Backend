d72f4793bcd7f713310abdce9e71b2119f3b8ed5
CristianOrizano <cristianorizano57@gmail.com>
2024-10-19 16:27:44 -0500

    mapper

--- Food.Application/Admin/Dtos/Categorias/Profiles/CategoriaProfile.cs
+++ Food.Application/Admin/Dtos/Categorias/Profiles/CategoriaProfile.cs
@@ -21,15 +21,29 @@ namespace Food.Application.Admin.Dtos.Categorias.Profiles
 
             //pagination
             CreateMap<PagedResult<Categoria>, PageResponse<CategoriaDto>>();
-
             CreateMap<CategoriaFoodSaveDto, Categoria>()
+                .ForMember(dest => dest.FoodMenus, opt => opt.MapFrom(src => src.Foods))
+                   .AfterMap((src, dest) =>
+                   {
+                       // Establece el estado a true para cada FoodMenu
+                       if (dest.FoodMenus != null)
+                       {
+                           foreach (var foodMenu in dest.FoodMenus)
+                           {
+                               foodMenu.Estado = true; // Asigna Estado a true
+                           }
+                       }
+                   });
+
+     
+            /*CreateMap<CategoriaFoodSaveDto, Categoria>()
                 .ForMember(dest => dest.FoodMenus, opt => opt.MapFrom(src => src.Foods.Select(food => new FoodMenu
                 {
                     // Mapeo de propiedades dentro de FoodMenuSaveDto a FoodMenu
                    Precio  = food.Precio,
                     Descripcion = food.Descripcion,
                     Estado = true
-                })));
+                }))); */
         }
 
 
--- Food.Application/Admin/Dtos/FoodMenus/FoodMenuSaveDto.cs
+++ Food.Application/Admin/Dtos/FoodMenus/FoodMenuSaveDto.cs
@@ -8,7 +8,6 @@ namespace Food.Application.Admin.Dtos.FoodMenus
 {
     public class FoodMenuSaveDto
     {
-        public int IdCategoria { get; set; }
         public string? Nombre { get; set; }
         public string? Descripcion { get; set; }
         public string? nombreImg { get; set; }
